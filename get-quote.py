158d29cc611ca458a3e5d9a40fc66d0ec75b99ae
mohnish <cmohnishkumar1205@gmail.com>
2020-09-23 21:37:12 +0530

    appended few texts

--- get-quote.py
+++ get-quote.py
@@ -3,12 +3,16 @@ def main():
   #print("Keep it logically awesome.")
 
   f = open("quotes.txt")
-  quotes = f.readlines()
+  quotes = f.read()
+  #f=open('quotes.txt','w')
+  #f.write('mohnish')
+  #f.write('krishna')
+  
   f.close()
-  last=13
+  last=len(quotes)-1
   rnd=random.randint(0,last)
 
-  print(quotes[rnd])
+  print(quotes)
 
 if __name__== "__main__":
   main()
